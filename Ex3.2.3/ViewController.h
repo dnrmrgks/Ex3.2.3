910a3ff886d9d3233a9d7e4e361b97d0163c700a
park sung chan <dnrmrgks@naver.com>
2014-01-06 13:09:42 +0900

    Initial Commit

---
+++ Ex3.2.3/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Ex3.2.3
+//
+//  Created by SDT-1 on 2014. 1. 6..
+//  Copyright (c) 2014년 T. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Ex3.2.3Tests/Ex3_2_3Tests.m
@@ -0,0 +1,34 @@
+//
+//  Ex3_2_3Tests.m
+//  Ex3.2.3Tests
+//
+//  Created by SDT-1 on 2014. 1. 6..
+//  Copyright (c) 2014년 T. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Ex3_2_3Tests : XCTestCase
+
+@end
+
+@implementation Ex3_2_3Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
